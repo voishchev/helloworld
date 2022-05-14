0b695c481946a0ab1e44d8105152e726411be038
voishchev <voishchev.phys@gmail.com>
2022-05-14 19:15:33 +0300

    my first commit

---
+++ 0_helloworld.playground/Contents.swift
@@ -0,0 +1,33 @@
+//
+// svo 03.05.2022
+// const and variables
+//
+let greetings = "Hello World!"
+var direction = "Left"
+direction = "Right"
+//print (direction)
+// DataTypes
+var number = -10.0
+var letsTry: UInt8 = 124
+var x = 5.47
+var y = 10
+var sum = Int(x)+y
+var x2: Double = 1.4577777777777
+var x3: Float = 1.4577777777777
+// Concatenation
+var cct = greetings + " " + direction
+// Interpolation
+//print("Hello \(direction) \(greetings)")
+var interpolation = "Hello \(direction) \(greetings)"
+//--------------------------------------------------------
+var isNight = true
+if isNight {
+    print("Guten Aben")
+} else {
+    print("Guten tag!")
+}
+
+
+
+
+
